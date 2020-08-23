978e96c5ef7ddbd16955fa781d0bb46c8e3358b8
Ivan <tanasov49@gmail.com>
2020-08-23 12:15:33 +0200

    it my project

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+var sym = Symbol();
+let something;
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = ['plum.png', 'onratechange.jpeg ', 'apple.bmp'];
+console.log(arr[0]);
+
+// alert("hello, world!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+// let answer = +prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(answer));
+// console.log(typeof(arr));
+// console.log(something);
+// console.log(4 / 0);
+// console.log('string' * 9);
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || isClose);
